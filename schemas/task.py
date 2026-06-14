dbf57fa478e43ced1d42172355b5e8a24027255a
JMLopera909 <jmloperah@correo.iue.edu.co>
2026-06-13 19:29:50 -0500

    Creating schemas/ and task schemas

---
+++ schemas/task.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+
+class TaskUpdate(BaseModel):
+    title: str
+    description: str
+    completed: bool
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
+    created_at: datetime
